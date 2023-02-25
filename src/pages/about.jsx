63d79c4257704ccc1bc39a142a2eac612223fb0a
mikyungyun <yun.workk@gmail.com>
2023-02-25 13:44:29 +0900

    fetching data

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import Title from '../components/Title';
+export default function About() {
+  return (
+    <div>
+      <Title title="About" />
+      <h1>About us</h1>
+    </div>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import Title from '../components/Title';
+
+const API_KEY = '237eb3eeefedbc6e1571ab715fec82a1';
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+  return (
+    <div>
+      <Title title={Home} />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
