99c2ae7536fb427631f3375a0bcaaa18fff14715
mikyungyun <yun.workk@gmail.com>
2023-02-25 14:00:48 +0900

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-contact/:path*",
+        destination: '/new-contact/:path*',
+        permanent: false,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
